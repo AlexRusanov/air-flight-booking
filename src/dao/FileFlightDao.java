2eec312b8914f857db2b566f28cc8c571bf73eeb
WhiteN <whiten.ncs@gmail.com>
2020-03-10 00:25:17 +0200

    fix not used

--- src/dao/FileFlightDao.java
+++ src/dao/FileFlightDao.java
@@ -6,7 +6,6 @@ import model.Flight;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
